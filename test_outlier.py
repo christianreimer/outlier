9ec81548b22cd7cdcbfbe9adda5395228ef4408d
Christian Reimer <creimer@mudbox.org>
2017-10-02 07:22:56 -0400

    Adding numpy based test

--- test_outlier.py
+++ test_outlier.py
@@ -1,16 +1,22 @@
 import random
+import numpy as np
 import outlier
 
 
 def test_no_outlier():
-    out = outlier.Outlier(11, 3)
-    for _ in range(100):
-        assert not out.add_and_check(1)
+    data = np.random.random(100)
+
+    out = outlier.Outlier(100, 5)
+    for d in data:
+        assert not out.add_and_check(d)
 
 
 def test_outlier():
-    out = outlier.Outlier(11, 3)
-    for _ in range(100):
-        out.add_and_check(random.random())
-    assert out.add_and_check(3)
+    data = np.random.random(100)
+    std = np.std(data)
 
+    out = outlier.Outlier(100, 3)
+    for d in data:
+        out.add(d)
+    should_be_outlier = np.mean(data) + 3.1 * std
+    assert out.check(should_be_outlier)
